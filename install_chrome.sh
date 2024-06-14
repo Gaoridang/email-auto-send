494bf0df9c8b3d5809b94507b5fc8f94ac96aad7
Lee JaeJun <monooi@naver.com>
2024-06-15 07:42:06 +0900

    Refactor: change to firefox

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Update and install necessary packages
-apt-get update
-apt-get install -y chromium chromium-driver
+echo "Starting Chrome installation" >> /tmp/install_chrome.log
 
-# Create a symbolic link to make chromedriver available in PATH
-ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
+# Install Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "Added Google Linux signing key" >> /tmp/install_chrome.log
+
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+echo "Installed Google Chrome" >> /tmp/install_chrome.log
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip -P /tmp
+unzip /tmp/chromedriver_linux64.zip -d /usr/bin
+chmod +x /usr/bin/chromedriver
+rm /tmp/chromedriver_linux64.zip
+
+echo "Installed ChromeDriver" >> /tmp/install_chrome.log
---
+++ streamlit_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Chrome
+./install_chrome.sh
+
+# Run Streamlit app
+streamlit run main.py
