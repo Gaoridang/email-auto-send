ca49edbfefdbd94941884625a2b2df3f3f866a40
Lee JaeJun <monooi@naver.com>
2024-06-15 09:20:44 +0900

    Update

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def get_page_title(url):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.binary_location = "/usr/bin/chromium-browser"
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
