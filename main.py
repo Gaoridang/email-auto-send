035bd2a3521e265b43f9074ed4e967a2fe63eb27
Lee JaeJun <monooi@naver.com>
2024-06-15 09:19:30 +0900

    Update

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def get_page_title(url):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
